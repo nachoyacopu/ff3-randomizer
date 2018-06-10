b92fe1a33a595e2c77db8cd3608b5f13e238aed8
Ignacio Cartes <ignacio.cartes.w@gmail.com>
2018-06-10 09:13:29 -0400

    Boulderless patch is now applied properly

--- js/ff3-randomizer/randomizer.js
+++ js/ff3-randomizer/randomizer.js
@@ -6,28 +6,28 @@
  *
  */
 
-var FF3 = (function(window, $, module, undefined) {
+var FF3 = (function (window, $, module, undefined) {
     'use strict';
-    
+
     var ROM;
-    
+
     // Some "global" utility functions
-    
+
     // tan random is a weighted random function that gives numbers between [-range to +range]
     // its weighted to be more likely to give numbers closer to 0 (less deviation)
-    module.tanRandom = function(range) {
+    module.tanRandom = function (range) {
         var r = (Math.random() - 0.5) * (3.1415296 / 2);
         return Math.pow(Math.tan(r), 2) * Math.sign(r) * range;
     };
-    
-    module.random_array_from = function(arr, size) {
+
+    module.random_array_from = function (arr, size) {
         var newArray = [];
-        for(var i=0;i<size;i++) {
+        for (var i = 0; i < size; i++) {
             newArray.push(arr[parseInt(Math.random() * arr.length)]);
-        };
+        }
         return newArray;
     };
-    
+
     /*
     function giveMeShopPointers() {
         var shopsPtrA = 0x583D6;
@@ -67,48 +67,51 @@ var FF3 = (function(window, $, module, undefined) {
         prettifyAddresses(itemShops);
         prettifyAddresses(magiShops);
     };*/
-    
-    
-    
-    module.randomizeROM = function(buffer) {
-        
+
+
+
+    module.randomizeROM = function (buffer) {
+
         // Convert buffer to array of bytes
         //var ROM = new Uint8Array(buffer);
-        ROM = new ByteArray(buffer).injectRandomizerMethods();
-        
+        ROM = new window.ByteArray(buffer).injectRandomizerMethods();
+
         // Balancing
         if ($('#chk-bal-jobs-stats').is(':checked'))
             module.balancing.balanceJobsStartingStats(ROM);
-            //balanceJobsStartingStats();
-        
+        //balanceJobsStartingStats();
+
         if ($('#chk-bal-jobs-commands').is(':checked')) {
             module.applyPatch("bard_improved_scare", ROM);
             module.applyPatch("bard_improved_cheer", ROM);
-        };
-        
+        }
+
         if ($('#chk-bal-not-so-defenseless').is(':checked'))
             module.applyPatch("not_so_defenseless", ROM);
-        
-        
+
+        if ($('#chk-patch-boulderless').is(':checked'))
+            module.applyPatch("boulderless", ROM);
+
+
         if ($('#chk-bal-items').is(':checked')) {
             // Ribbon price: 60000
             ROM.setInt16(60000, 0x21EF2);
-        };
-        
+        }
+
         // Jobs
         //generateJobsPool();
         module.jobs.generateJobsPool();
         if ($('#chk-jobs-shuffle').is(':checked'))
             module.jobs.shuffleJobs(ROM);
-            //shuffleJobs();
-        
+        //shuffleJobs();
+
         // Items
         module.items.generateItemPool();
         if ($('#chk-items-chests').is(':checked'))
             module.items.randomizeChests(ROM);
         if ($('#chk-items-dropsteals').is(':checked'))
             module.items.randomizeDropsSteals(ROM);
-        
+
         // Shops
         //giveMeShopPointers(ROM);
         if ($('#chk-shops-weapon').is(':checked'))
@@ -122,11 +125,11 @@ var FF3 = (function(window, $, module, undefined) {
 
         // Equipment
         module.items.randomizeEquipment(ROM);
-        
-        
+
+
         // Monsters
         module.monsters.handleMonsterRandomization(ROM);
-        
+
         // Boosts
         if ($('#chk-boost-exp').is(':checked'))
             module.misc.boostExp(ROM);
@@ -136,11 +139,11 @@ var FF3 = (function(window, $, module, undefined) {
             module.misc.boostCapacity(ROM);
         if ($('#chk-boost-skill').is(':checked'))
             ROM[module.address.skillPointsPerLevel] = 50;
-        
+
         // Hardcore
         if ($('#chk-hc-encounters').is(':checked'))
             module.monsters.randomizeEncounterGroupsByArea(ROM);
-        
+
         // Visual
         if ($('#chk-ve-menu').is(':checked'))
             module.visual.randomizeMenuColor(ROM);
@@ -150,44 +153,44 @@ var FF3 = (function(window, $, module, undefined) {
             module.visual.randomizeWeaponAnimations(ROM);
         if ($('#chk-ve-weapon-palettes').is(':checked'))
             module.visual.randomizeWeaponPalettes(ROM);
-        
-        
-        
+
+
+
         // Misc
         if ($('#chk-misc-saveanywhere').is(':checked'))
             ROM[0x7A5B5] = 0x00;
-        
+
         if ($('#chk-misc-steptable').is(':checked'))
             module.misc.randomizeStepTable(ROM);
-        
+
         if ($('#chk-half-encounter-rate').is(':checked'))
             module.misc.halfEncounterRate(ROM);
-        
+
         if ($('#chk-all-encounters-runnable').is(':checked'))
             module.misc.allEncountersRunnable(ROM);
-        
+
         if ($('#chk-misc-movespeed').is(':checked')) {
             ROM[module.address.moveSpeed] = 0x02;
             ROM[module.address.moveSpeed2] = 0x02;
             ROM[module.address.moveSpeed3] = 0x02;
-        };
-        
+        }
+
         if ($('#chk-misc-movespeed-battle').is(':checked')) {
             ROM[module.address.moveSpeedBattle] = 0xFE;
             ROM[module.address.moveSpeedBattle2] = 0x02;
-        };
-        
+        }
+
         if ($('#chk-misc-fatchocobo').is(':checked'))
             ROM[0x7B4D2] = 0x0D;
-        
+
         // Number of crystal jobs
         if ($('#chk-misc-morejobs').is(':checked')) {
             ROM[module.address.numOfCrystalJobs[0]] = 7;
             ROM[module.address.numOfCrystalJobs[1]] = 11;
             ROM[module.address.numOfCrystalJobs[2]] = 15;
             ROM[module.address.numOfCrystalJobs[3]] = 19;
-        };
-        
+        }
+
         // Debug
         if ($('#chk-debug-feoks').is(':checked')) {
             ROM.setInt16(9999, 0x73BE8);
@@ -198,11 +201,11 @@ var FF3 = (function(window, $, module, undefined) {
             ROM[0x73C04] = 0x01;
             ROM[0x73C05] = 0x59;
             ROM[0x73C06] = 0x01;
-        };
-        
+        }
+
         return ROM;
-        
+
     };
-    
+
     return module;
 })(window, jQuery, FF3 || {});
