9d9bcdc4050835e3f9afe6bee83381fac6662e38
Ignacio Cartes <ignacio.cartes.w@gmail.com>
2018-06-10 09:05:09 -0400

    Add boulderless patch

--- js/ff3-randomizer/patcher.js
+++ js/ff3-randomizer/patcher.js
@@ -6,12 +6,29 @@
  *
  */
 
-var FF3 = (function(window, $, module, undefined) {
+var FF3 = (function (window, $, module, undefined) {
     'use strict';
-    
+
     // All of the patches:
-    
+
     var patches_data = {
+
+        
+        /* Boulderless:
+         *
+         *  ...removes the boulder blocking the path outside of the starting continent
+         *  (still needs testing to ensure stuff won't break later in the game)
+         */
+        "boulderless": [
+            {
+                address: 0x7C617,
+                data: [0x18, 0x60, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA]
+            },
+            {
+                address: 0x7D547,
+                data: [0x60, 0xEA]
+            }
+        ],
         
         /* Improved Scare:
          *
@@ -22,7 +39,7 @@ var FF3 = (function(window, $, module, undefined) {
             address: 0x6AD43,
             data: [0xB1, 0x6E, 0x4A, 0x85, 0x24]
         },
-        
+
         /* Improved Cheer:
          *
          *  ...from: Cheer increases party attack power by 10
@@ -32,7 +49,7 @@ var FF3 = (function(window, $, module, undefined) {
             address: 0x6AD9E,
             data: [0xA0, 0x00, 0xB1, 0x6E, 0x0A, 0xA0, 0x19, 0x18, 0x71, 0x18, 0x90, 0x02, 0xA9, 0xFF]
         },
-        
+
         /* Not So Defenseless:
          *
          *  ...from: Defenseless sets your defense to 0
@@ -42,27 +59,39 @@ var FF3 = (function(window, $, module, undefined) {
             address: 0x62083,
             data: [0x46, 0x26, 0xE6, 0x26]
         },
-        
+
     };
+
     
-    
+
     // Function to apply patches
-    
+
     module.applyPatch = function (patchId, ROM) {
-        
+
         // Find if patch exists
         if (patchId in patches_data) {
-            // Convert data to Uint8Array and apply to ROM
-            ROM.set(new Uint8Array(patches_data[patchId].data), patches_data[patchId].address);
+            var patch = patches_data[patchId];
+            // if patch is an array, then it contains multiple patches to iterate through
+            if (Array.isArray(patch)) {
+                for (var index in patch) {
+                    var subpatch = patch[index];
+                    // Convert data to Uint8Array and apply to ROM
+                    ROM.set(new Uint8Array(subpatch.data), subpatch.address);
+                }
+            } else {
+                // if it is an object, its just one patch, apply it
+                // Convert data to Uint8Array and apply to ROM
+                ROM.set(new Uint8Array(patch.data), patch.address);
+            }
             module.log("Patch [" + patchId + "] applied!");
-            
+
         } else {
             module.log("Patch [" + patchId + "] does not exist!");
-        };
-        
+        }
+
     };
-    
-    
-    
+
+
+
     return module;
 })(window, jQuery, FF3 || {});
